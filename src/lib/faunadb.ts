78cb3fd3df5ec53fd97faa87d6379f9801a51792
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2021-04-25 19:41:18 +0200

    :hammer: chore: fix build

--- src/lib/faunadb.ts
+++ src/lib/faunadb.ts
@@ -1,3 +1,7 @@
 import { Client } from "faunadb";
 
+if (!process.env.FAUNA_KEY) {
+  throw new Error("`FAUNA_KEY` is required!");
+}
+
 export const client = new Client({ secret: process.env.FAUNA_KEY });
